1a75f32da452f585ed9bae2c6f800f9169ffb41b
erastov-alex <erastov.alex@gmail.com>
2023-09-25 18:17:09 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Off-Road Terrain for Autonomous Vehicles"
 PROJECT_NAME_FULL: str = "Off-Road Terrain for Autonomous Vehicles"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
