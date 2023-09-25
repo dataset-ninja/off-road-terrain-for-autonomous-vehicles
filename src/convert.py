ce90b5503711366d41462b53f432883533affc42
erastov-alex <erastov.alex@gmail.com>
2023-09-25 11:13:05 +0300

    upd gitignore, settings, convert

--- src/convert.py
+++ src/convert.py
@@ -1,9 +1,11 @@
 import supervisely as sly
 import os
+import csv
+from collections import defaultdict
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
 import shutil
 
 from tqdm import tqdm
@@ -70,16 +72,161 @@ def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    images_path = os.path.join("Off-Road Terrain Dataset for Autonomous Vehicles","Images","Images")
+    sensors_path = os.path.join("Off-Road Terrain Dataset for Autonomous Vehicles","SensorData")
+    # masks_path = "/home/alex/DATASETS/TODO/Off-Road Terrain Attention/archive/AttentionRegion/AttentionRegion/light"
+    tsm1_labels_path = os.path.join("Off-Road Terrain Dataset for Autonomous Vehicles","ImageLabels","tsm_1_labels.csv")
+    tsm2_labels_path = os.path.join("Off-Road Terrain Dataset for Autonomous Vehicles","ImageLabels","tsm_2_labels.csv")
+    ds_name = "ds"
+    batch_size = 50
+    masks_ext = ".png"
 
-    # ... some code here ...
+    proj_dict = defaultdict()
+    possible_tags = []
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    for seq in os.listdir(sensors_path):
+        img_seq = seq[:10]
+        accelerometer = {}
+        gps = {}
+        gyroscope = {}
+        magnetometer = {}
+        record = {}
+        file_name_to_dict = {
+            "accelerometer_calibrated_split.csv": accelerometer,
+            "gps.csv": gps,
+            "gyroscope_calibrated_split.csv": gyroscope,
+            "magnetometer_split.csv": magnetometer,
+            "record.csv": record,
+        }
+        file_name_to_dict = {
+        "accelerometer_calibrated_split.csv": accelerometer,
+        "gps.csv": gps,
+        "gyroscope_calibrated_split.csv": gyroscope,
+        "magnetometer_split.csv": magnetometer,
+        "record.csv": record,
+        }
 
-    # return project
+        curr_sensors_path = os.path.join(sensors_path, seq)
+        for sensor_name in list(file_name_to_dict.keys()):
+            file_path = os.path.join(curr_sensors_path, sensor_name)
+            with open(file_path, "r") as file:
+                csvreader = csv.reader(file)
+                for idx, row in enumerate(csvreader):
+                    if idx != 0:
+                        file_name_to_dict[sensor_name]["{}s{}ms".format(row[1], str(int(float((row[2])))))] = {name:[row[i+3]] for i, name in enumerate(dict_names[3:])}
+                    else: 
+                        dict_names = [name for name in row]
+                        possible_tags.extend(dict_names)
+        proj_dict[img_seq] = file_name_to_dict
+    
+
+    def create_ann(image_path):
+        labels = []
+        tags = []
+        img_height = 2160
+        img_wight = 3840
+
+        head,filename = os.path.split(image_path)
+        filename = filename[:-4]
+
+        tag_s = sly.Tag(tag_seq, value=os.path.basename(head))
+        tags.append(tag_s)
+
+        tsm1_data = name_to_tsm1.get(get_file_name_with_ext(image_path))
+        if tsm1_data is not None:
+            for cat in tsm1_data:
+                tag_name = cat
+                tag_value = str(tsm1_data[cat])
+                tag = [sly.Tag(tagmeta, value=tag_value) for tagmeta in tag_metas if tagmeta.name == tag_name]
+                tags.extend(tag)
+        tsm2_data = name_to_tsm2.get(get_file_name_with_ext(image_path))
+        if tsm2_data is not None:
+            for cat in tsm2_data:
+                tag_name = cat
+                tag_value = str(tsm2_data[cat])
+                tag = [sly.Tag(tagmeta, value=tag_value) for tagmeta in tag_metas if tagmeta.name == tag_name]
+                tags.extend(tag)
+
+        timestamp_and_timestamp_ms = False
+
+        for csvfile in proj_dict[os.path.basename(head)]:
+            try:
+                for cat in proj_dict[os.path.basename(head)][csvfile][filename]:
+                    if cat:
+                        if timestamp_and_timestamp_ms and "timestamp" in cat:
+                            continue
+                        if "timestamp_ms" in cat and not timestamp_and_timestamp_ms:
+                            timestamp_and_timestamp_ms = True
+                        tag_value = proj_dict[os.path.basename(head)][csvfile][filename][cat]
+                        if isinstance(tag_value,list):
+                            tag_value = str(tag_value[0])
+                        else: 
+                            tag_value = str(tag_value)
+                        tag_name = cat
+                        tag = [sly.Tag(tagmeta, value=tag_value) for tagmeta in tag_metas if tagmeta.name == tag_name]
+                        tags.extend(tag)
+            except KeyError:
+                pass
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+
+    possible_tags.extend(["tsm1_original","tsm1_k2","tsm1_k3","tsm1_k4","tsm2_original","tsm2_k2","tsm2_k3","tsm2_k4"])
+    possible_tags = set(possible_tags)
+    tag_metas = [sly.TagMeta(name, sly.TagValueType.ANY_STRING) for name in possible_tags if name and "utc_" not in name]
+    tag_seq = sly.TagMeta("sequence", sly.TagValueType.ANY_STRING)
+    tag_metas.extend([tag_seq])
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        tag_metas=tag_metas,
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    name_to_tsm1 = {}
+    with open(tsm1_labels_path, "r") as file:
+        csvreader = csv.reader(file)
+        for idx, row in enumerate(csvreader):
+            if idx != 0:
+                name_to_tsm1[row[0]] = {"tsm1_original": row[1], "tsm1_k2": row[2], "tsm1_k3": row[3], "tsm1_k4": row[4]}
+
+    name_to_tsm2 = {}
+    with open(tsm2_labels_path, "r") as file:
+        csvreader = csv.reader(file)
+        for idx, row in enumerate(csvreader):
+            if idx != 0:
+                name_to_tsm2[row[0]] = {"tsm2_original": row[1], "tsm2_k2": row[2], "tsm2_k3": row[3], "tsm2_k4": row[4]}
+
+    for subfolder in os.listdir(images_path):
+        curr_images_path = os.path.join(images_path, subfolder)
+        accelerometer = {}
+        gps = {}
+        gyroscope = {}
+        magnetometer = {}
+        record = {}
+        
+        images_names = os.listdir(curr_images_path)
+
+        progress = sly.Progress(
+            "Create dataset {}, add {} data".format(ds_name, subfolder), len(images_names)
+        )
+
+        for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+            images_pathes_batch = [
+                os.path.join(curr_images_path, image_path) for image_path in img_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns_batch)
+
+            progress.iters_done_report(len(img_names_batch))
+    
+    return project
+
 
 
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,37 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Off-Road Terrain for Autonomous Vehicles"
+PROJECT_NAME_FULL: str = "Off-Road Terrain for Autonomous Vehicles"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Automotive()]
+CATEGORY: Category = Category.SelfDriving()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.Classification()]
+ANNOTATION_TYPES: List[AnnotationType] = []
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2021
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/magnumresearchgroup/offroad-terrain-dataset-for-autonomous-vehicles"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 4944149
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/off-road-terrain-for-autonomous-vehicles"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/magnumresearchgroup/offroad-terrain-dataset-for-autonomous-vehicles"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,18 +51,18 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://www.dre.vanderbilt.edu/~schmidt/PDF/paper_1.pdf"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
 REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://www.kaggle.com/datasets/magnumresearchgroup/offroad-terrain-dataset-for-autonomous-vehicles"
+AUTHORS: Optional[List[str]] = ["Gabriela Gresenz", "Jules White", "Douglas C. Schmidt"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = ["Vanderbilt University"]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = ["https://www.vanderbilt.edu/"]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {"__POSTTEXT__":"Dataset contatins the corresponding sensor data from a global positioning system (GPS, inertial measurement units (IMUs), and a wheel rotation speed sensor, z-axis acceleration"}
 TAGS: Optional[List[str]] = None
 
 
